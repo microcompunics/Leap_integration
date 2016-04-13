b1b99d5dc682a309103ee876065d1644fd5ac290
Daniel Christensen <danielwow2103@gmail.com>
2016-04-13 14:41:41 +0200

    Leap integration
    Hands folder, with scripts
    Script folder with scripts

---
+++ Scripts/Hands/HandEnableDisable.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class HandEnableDisable : HandTransitionBehavior {
+
+    public GameObject inhand;
+    public Vector3 pos;
+
+	protected override void HandReset() {
+        gameObject.SetActive(true);
+
+        /*if (inhand != null)
+        {
+            //inhand.transform.position = GameObject.Find("palm").transform.position;
+            //inhand.transform.parent = GameObject.Find("palm").transform;
+            inhand.transform.localPosition = pos;
+            
+        }*/
+
+    }
+
+	// Use this for initialization
+	protected override void HandFinish () {
+        
+
+       /* if (GameObject.Find("palm").transform.childCount != 0)
+        {
+
+            inhand = GameObject.Find("palm").transform.GetChild(0).gameObject;
+            //pos = inhand.transform.localPosition;
+            pos = new Vector3(inhand.transform.localPosition.x, inhand.transform.localPosition.y, inhand.transform.localPosition.z);
+        }*/
+        
+
+            gameObject.SetActive(false);
+
+        
+   
+    }
+	
+}
